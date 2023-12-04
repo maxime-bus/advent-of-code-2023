c02b038a97bc3dad7bc7737d3ef798b7004600d3
Maxime BUS <bus.maxime@mailoo.org>
2023-12-04 19:00:25 +0100

    Réponse jour 4 - partie 2

--- day04.hs
+++ day04.hs
@@ -1,12 +1,18 @@
 import           Data.List.Split
+import qualified Data.Map        as Map
+import           Data.Maybe
 
 type WinningNumbers = [Int]
 type ScratchCardNumbers = [Int]
 type Winners = [Int]
-type ScratchCard = (WinningNumbers, ScratchCardNumbers)
+type Id = Int
+type ScratchCard = (Id, WinningNumbers, ScratchCardNumbers)
+
+cardId :: ScratchCard -> Id
+cardId (id, _, _) = id
 
 winners :: ScratchCard -> Winners
-winners (winningNumbers, numbers) = filter (\ wn -> wn `elem` numbers) winningNumbers
+winners (_, winningNumbers, numbers) = filter (\ wn -> wn `elem` numbers) winningNumbers
 
 winnersScore :: Winners -> Int
 winnersScore []  = 0
@@ -16,15 +22,37 @@ winnersScore n   = 2 * (winnersScore $ tail n)
 pointsForCard :: ScratchCard -> Int
 pointsForCard = winnersScore . winners
 
+hasWon :: ScratchCard -> Bool
+hasWon = (/= 0) . pointsForCard
+
 parseCardLine :: String -> ScratchCard
 parseCardLine line =
   let [firstHalf, secondHalf] = splitOn "|" line
-      [_, winningNumbers] = splitOn ":" firstHalf
-  in (map read $ words winningNumbers, map read $ words secondHalf)
+      [cardId, winningNumbers] = splitOn ":" firstHalf
+  in (read $ last $ words cardId, map read $ words winningNumbers, map read $ words secondHalf)
 
+totalGainsForOneScratchCard :: Id -> Map.Map Id Winners -> Int
+totalGainsForOneScratchCard cardId cards =
+  case Map.lookup cardId cards of
+    Nothing -> 0
+    Just winners -> do
+      let numberOfWinners = length winners
+          nextCardsIds = [cardId + 1..cardId + numberOfWinners]
 
+      numberOfWinners + (sum $ map (\ nextCardId -> totalGainsForOneScratchCard nextCardId cards) nextCardsIds)
+
+totalGainsForAllScratchCards :: [ScratchCard] -> Map.Map Id Winners -> Int
+totalGainsForAllScratchCards scratchCards m = length scratchCards + (sum $ map (\ cardId -> totalGainsForOneScratchCard cardId m) $ map cardId scratchCards)
+
+computeMapOfScratchCards :: [ScratchCard] -> Map.Map Id Winners
+computeMapOfScratchCards = foldr (\ scratchCard acc -> Map.insert (cardId scratchCard) (winners scratchCard) acc) Map.empty
 
 main = do
   input <- lines <$> readFile "day04.input"
 
-  print $ sum $ map pointsForCard $ map parseCardLine input
+  let scratchCards = map parseCardLine input
+      mapOfScratchCards = computeMapOfScratchCards scratchCards
+
+  print $ sum $ map pointsForCard scratchCards
+
+  print $ totalGainsForAllScratchCards scratchCards mapOfScratchCards
